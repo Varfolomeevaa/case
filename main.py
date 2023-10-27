2e7867e4629df285b9d8c73d177d1fe9442187a1
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-10-27 18:15:15 +0700

    turtle

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import turtle
+
+
 def get_color_choice():
+    """
+    function for selecting hexagon fill color
+    :return: hexagon fill color
+    """
     print('Доступные цвета заливки:')
     print('красный', 'желтый', 'синий', 'зелёный', 'черный', 'розовый', sep='\n')
     colors = ['красный', 'желтый', 'синий', 'зелёный', 'черный', 'розовый']
