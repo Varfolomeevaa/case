9bcb20c9e32c16253f4f2f44c4f899e43c4eaaf2
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-10-27 18:02:42 +0700

    get_color_choice_

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-def get_color_choice():
-    print('Доступные цвета заливки:')
-    print('красный', 'желтый', 'синий', 'зелёный', 'черный', 'розовый', sep='\n')
-    colors = ['красный', 'желтый', 'синий', 'зелёный', 'черный', 'розовый']
-    flag = False
-    while not flag:
-        color = input('Пожалуйста, введите цвет: ').lower()
-        if color in colors:
-            return color
-        if not flag:
-            print(color, 'не является верным значением.')
+
 
 
